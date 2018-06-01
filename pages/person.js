07bd0551928f37a691e6f865ef95e404a0851caa
PHUA XUE YONG _ <xyphua.2014@smu.edu.sg>
2018-06-01 13:08:50 +0800

    set up basic components

--- pages/person.js
+++ pages/person.js
@@ -16,13 +16,9 @@ import {
   Visibility,
   Progress
 } from "semantic-ui-react";
-const axios = require("axios");
 
 export default class Person extends Component {
   static async getInitialProps({ id, person }) {
-    // call the api to get all data
-    // return the data as props. Add those props into state to be rendered through iteration.
-    // See if you can get a transition to display those profiles in a seperate segment, parallal to the main page
     return {};
   }
 
@@ -35,12 +31,8 @@ export default class Person extends Component {
     let {} = this.state;
     return (
       <div>
-        <Header as="h1" content="Welcome, Commander" />
         <Segment>
-          <Header as="h2" />
-          <List>
-            <List.Item>test</List.Item>
-          </List>
+          <Header as="h1" />
         </Segment>
       </div>
     );
